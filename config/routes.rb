09030e4a70f9131f56b4e59e408f74e79a711d6a
jesusalc <jesusalc@gmail.com>
2016-07-25 12:16:08 +0200

    added routes to respond to v1/ requests to the routing file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  contraints subdomain 'api' do
+    scope module: 'api' do
+      namespace :v1 do
+        resource :users
+      end
+    end
+  end  
+
 end
