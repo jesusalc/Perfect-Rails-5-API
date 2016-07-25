076d6b432ff6fdb36d85233374cc3e09ee1b5b0c
jesusalc <jesusalc@gmail.com>
2016-07-25 15:28:41 +0200

    corrected routes file typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
+  #resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  contraints subdomain 'api' do
+  constraints subdomain: 'api' do
     scope module: 'api' do
       namespace :v1 do
-        resource :users
+        resources :users
       end
     end
-  end  
-
+  end
 end
