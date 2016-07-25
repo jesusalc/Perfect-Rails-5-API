6f67de7c84bcb79234d4f2f0e9b3231d25642c1e
jesusalc <jesusalc@gmail.com>
2016-07-25 12:30:04 +0200

    corrected O to 0 typo

--- config/application.rb
+++ config/application.rb
@@ -28,13 +28,12 @@ module ApiAppName
     config.api_only = true
 
     config.middleware.use Rack::Attack
-    config.middleware.insert_before O, "Rack::Cors" do
-	    allow do
-		    origins '*' 
-		    resource '*', :headers => :any, :methods => [:get, :post, :options]
-	    end
+    config.middleware.insert_before 0, "Rack::Cors" do
+	allow do
+		origins '*'
+		resource '*', :headers => :any, :methods => [:get, :post, :options]
+	end
     end
-
 
   end
 end
